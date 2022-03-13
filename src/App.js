d44ae149c466aa7003c63ac42bf20358df84a2cf
Alaa Abou Arraj <88724459+3la2011@users.noreply.github.com>
2022-03-13 21:18:06 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tasks: [{ title: "task1", detail: "detail", id: "task1" }],
+      tasks: [
+        { title: "task1", detail: "detail", id: "task1" },
+        { title: "task2", detail: "detail2", id: "task2" },
+      ],
       isEditOpen: false,
       taskClicked: "",
     };
     this.handleToggleEdit = this.handleToggleEdit.bind(this);
-    this.addTask = this.addTask.bind(this);
+    this.handleAddTask = this.handleAddTask.bind(this);
     this.handleSaveEditedTask = this.handleSaveEditedTask.bind(this);
+    this.handleDeleteTask = this.handleDeleteTask.bind(this);
   }
-  addTask() {
+  handleAddTask() {
     this.setState({
       tasks: [
         ...this.state.tasks,
@@ -30,24 +34,53 @@ class App extends React.Component {
   }
 
   handleToggleEdit(e) {
+    e
+      ? this.setState({
+          isEditOpen: !this.state.isEditOpen,
+          taskClicked: this.state.tasks.filter(
+            (task) => task.id === e.target.id
+          )[0],
+        })
+      : this.setState({
+          isEditOpen: !this.state.isEditOpen,
+        });
+  }
+
+  handleDeleteTask(taskId) {
+    const indexEditedTask = this.state.tasks
+      .map((elem) => elem.id)
+      .indexOf(taskId);
+    console.log(indexEditedTask, this.state.tasks);
     this.setState({
-      isEditOpen: !this.state.isEditOpen,
-      taskClicked: this.state.tasks.filter(
-        (task) => task.id === e.target.id
-      )[0],
+      tasks: [
+        ...this.state.tasks.slice(0, indexEditedTask),
+        ...this.state.tasks.slice(indexEditedTask + 1, this.state.tasks.length),
+      ],
     });
+    console.log(this.state.tasks);
+    this.handleToggleEdit();
   }
 
   handleSaveEditedTask(newValues) {
-    const indexEditedTask = this.state.tasks.indexOf(newValues.id)[0];
-    console.log(newValues);
-    this.setState({ tasks: [...this.state.tasks.splice()] });
+    const indexEditedTask = this.state.tasks
+      .map((elem) => elem.id)
+      .indexOf(newValues.id);
+    this.setState({
+      tasks: [
+        ...this.state.tasks.slice(0, indexEditedTask),
+        ...this.state.tasks.slice(indexEditedTask + 1, this.state.tasks.length),
+        newValues,
+      ],
+    });
+    this.handleToggleEdit();
   }
   render() {
     const editPage = this.state.isEditOpen ? (
       <EditPage
         task={this.state.taskClicked}
+        handleToggleEdit={this.handleToggleEdit}
         handleSaveEditedTask={this.handleSaveEditedTask}
+        handleDeleteTask={this.handleDeleteTask}
       />
     ) : (
       ""
@@ -60,7 +93,7 @@ class App extends React.Component {
           key={this.state.tasks.id}
           handleToggleEdit={this.handleToggleEdit}
         />
-        <button id="addTaskBtn" onClick={() => this.addTask()}>
+        <button id="addTaskBtn" onClick={() => this.handleAddTask()}>
           +
         </button>
         {editPage}
--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -7,12 +7,15 @@ class EditPage extends React.Component {
       task: props.task,
     };
     this.handleSaveEditedTask = (value) => props.handleSaveEditedTask(value);
+    this.handleDeleteTask = (value) => props.handleDeleteTask(value);
+    this.handleToggleEdit = () => props.handleToggleEdit();
   }
 
   render() {
     console.log(this.state.task);
     return (
       <div className="EditPage flex col">
+        <div className="closeEdit" onClick={() => this.handleToggleEdit()} />
         <textarea className="textarea flex" id="textareaTitle">
           {this.state.task.title}
         </textarea>
@@ -24,11 +27,21 @@ class EditPage extends React.Component {
           <button
             className="btn"
             id="save"
-            onClick={() => this.handleSaveEditedTask({ id: "task1" })}
+            onClick={() =>
+              this.handleSaveEditedTask({
+                title: document.getElementById("textareaTitle").value,
+                detail: document.getElementById("textareaDetail").value,
+                id: this.state.task.id,
+              })
+            }
           >
             Save
           </button>
-          <button className="btn" id="delete">
+          <button
+            className="btn"
+            id="delete"
+            onClick={() => this.handleDeleteTask(this.state.task.id)}
+          >
             Delete
           </button>
         </div>
